b00052beca7da913b54f75c89a085049cca6e150
Joshua <joshuagpatten@gmail.com>
2022-03-04 21:35:08 -0500

    finish exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, inputNumber) {
+    let answer = '';
+    if(inputNumber >= 0){
+        for(let i = 0; i < inputNumber; i++){
+            answer = answer + inputString;
+        }
+    return answer;
+    }
+    else{return 'ERROR';}
 };
 
 // Do not edit below this line
