50d5751c36e47fc5d3ca5fd1762ae41ec76ce51c
Joshua <joshuagpatten@gmail.com>
2022-03-05 21:58:32 -0500

    finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray) {
+    let args = Array.from(arguments);
+    let argsLength = args.length;
+    for(let j = 1; j<argsLength; j++){
+        inputArray = inputArray.filter(item => item !== args[j]);
+    }
+    return inputArray;
 };
 
 // Do not edit below this line
