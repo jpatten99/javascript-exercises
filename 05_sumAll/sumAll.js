3de6e722348e692eed8dee561ec12c3eab2e2adb
Joshua <joshuagpatten@gmail.com>
2022-03-05 22:29:06 -0500

    finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(firstInt, secondInt) {
+    let sum = 0;
+    if (!Number.isInteger(firstInt) || !Number.isInteger(secondInt)) return "ERROR";
+        if(firstInt > 0 && secondInt > 0){
+            if(firstInt < secondInt){
+                for(firstInt; firstInt <= secondInt; firstInt++){
+                    sum += firstInt;
+                }
+            }
+            else if(firstInt > secondInt){
+                for(secondInt; secondInt <= firstInt; secondInt++){
+                    sum += secondInt;
+                }
+            }    
+            return sum;
+        }
+        else{return "ERROR";}    
 };
 
 // Do not edit below this line
