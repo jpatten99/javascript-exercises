57fdd091b65e82839fa184553008fbb8eaea2286
Joshua <joshuagpatten@gmail.com>
2022-03-07 10:54:32 -0500

    Finish exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(inputOne, inputTwo) {
+	return inputOne + inputTwo;
 };
 
-const subtract = function() {
-	
+const subtract = function(inputOne, inputTwo) {
+  return inputOne - inputTwo;
 };
 
-const sum = function() {
-	
+const sum = function(inputArray) {
+  let sum = 0;
+  for(const numbers of inputArray){
+    sum +=Number(numbers);
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(inputArray) {
+  let answ = 1;
+  for(const numbers of inputArray){
+    answ *=numbers;
+  }
+  return answ;
 };
 
-const power = function() {
-	
+const power = function(inputOne, inputTwo) {
+  return Math.pow(inputOne, inputTwo);
 };
 
-const factorial = function() {
-	
+const factorial = function(inputNum) {
+  let answ=1;
+    for (let i = 2; i <= inputNum; i++)
+        answ = answ * i;
+    return answ;
 };
 
 // Do not edit below this line
