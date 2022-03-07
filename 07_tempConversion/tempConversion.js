28462d41b8299e4c5e6a84c70b70028002f19411
Joshua <joshuagpatten@gmail.com>
2022-03-07 10:26:06 -0500

    finish exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(inputTemp) {
+  return Math.round(((inputTemp - 32) * (5/9)) * 10) / 10;
 };
 
-const ctof = function() {
+const ctof = function(inputTemp) {
+  return Math.round((inputTemp * (9/5) + 32) * 10) / 10;
 
 };
 
