af92e33ea62ce38721dd59a63f72a78d7a801655
Joshua <joshuagpatten@gmail.com>
2022-03-07 10:17:22 -0500

    finish exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(inputYear) {
+    if(inputYear % 4 ===0){
+        if(inputYear % 100 === 0 && inputYear % 400 !== 0){
+            return false;
+        }
+        return true;    
+    }
+    return false;    
 };
 
 // Do not edit below this line
