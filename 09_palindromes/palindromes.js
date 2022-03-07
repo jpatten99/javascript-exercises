f9ee7123bc412419acea5a85e093889bc9df13b3
Joshua <joshuagpatten@gmail.com>
2022-03-07 11:26:01 -0500

    finish exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (inputString) {
+    inputString = inputString.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    //^^removes punctuation
+    inputString = inputString.replace(/\s+/g, '')
+    //^^removes whitespace
+    inputString = inputString.toLowerCase();
+    let reverseStr = reverseString(inputString);
+    return reverseStr === inputString;
 };
 
+function reverseString(str) {
+    if (str === "")
+      return "";
+    
+    else
+      return reverseString(str.substr(1)) + str.charAt(0);
+}
 // Do not edit below this line
 module.exports = palindromes;
