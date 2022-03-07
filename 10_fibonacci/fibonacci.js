875c398afdcbc8c0bd10f9482b56758a6ab93ce0
Joshua <joshuagpatten@gmail.com>
2022-03-07 11:51:17 -0500

    finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(inputVal) {
+    inputVal = Number(inputVal);
+    if(inputVal < 0){return 'OOPS';}
+    else if(inputVal === 0){return 0;}
+    else if (inputVal === 1){return 1;}
+    else{
+        let first = 0;
+        let second = 1;
+        let answ;
+        for(let i = 2; i <= inputVal; i++){
+            answ = first + second;
+            first = second;
+            second = answ;
+        }
+        return answ;
+    }
+    
 };
 
 // Do not edit below this line
